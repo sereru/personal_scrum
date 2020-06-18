f47a62f8d53d3ff77449b103ad5e90a4b6c6a1a7
Yamamoto Takaya <t-yamamoto@yamamotonoMacBook-Air.local>
2020-06-18 16:16:05 +0900

    New tag

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,24 @@
 class TagsController < ApplicationController
   def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      redirect_to controller: 'kanbans', action: 'new'
+    else
+      render 'tags/new'
+    end
   end
 
   def show
   end
+
+private
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
+
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ApplicationRecord
+  validates :name, presence: true
   has_many :kanban_tag_relations, dependent: :destroy
   has_many :kanbans, through: :kanban_tag_relations, dependent: :destroy
 end
